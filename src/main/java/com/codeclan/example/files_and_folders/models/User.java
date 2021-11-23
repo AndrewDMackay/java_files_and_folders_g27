bee8062a9120f31ef330600a68ea66a756044ea1
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-23 14:34:16 +0000

    AM, 1st commit main

--- src/main/java/com/codeclan/example/files_and_folders/models/User.java
+++ src/main/java/com/codeclan/example/files_and_folders/models/User.java
@@ -1,5 +1,36 @@
 
 package com.codeclan.example.files_and_folders.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private String name;
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
